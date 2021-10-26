36db32c0beb24b634e6a901cf97f0789fd09ea67
LeoLint <maniacalscream@gmail.com>
2021-10-26 13:10:10 +0300

    some fix
    some fix

--- js/main.js
+++ js/main.js
@@ -3,6 +3,15 @@ $('.slider').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2500,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 
   
